25682d966bd406e2b1ba0995d3b4e80518514af0
GuiSantos <Gui,santos0625@gmail.com>
2024-03-12 12:07:34 -0300

    botao

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
@@ -8,7 +8,9 @@ export default function App() {
       <Text>05/03/2024</Text>
       <Text>clone</Text>
       <StatusBar style="auto" />
+      <Button title='salvar'></Button>
     </View>
+   
   );
 }
 
